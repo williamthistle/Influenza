57394d92c82c1c27110d18ae29334a3968ff3215
William Thistlethwaite <williamthistle@gmail.com>
2023-09-08 13:48:13 -0400

    Update differential_expression.R

--- Vaccitech_Paper/extra_functions/rna/differential_expression.R
+++ Vaccitech_Paper/extra_functions/rna/differential_expression.R
@@ -27,63 +27,6 @@ run_differential_expression_cluster <- function(sc_obj, marker_dir) {
   message("All done!")
 }
 
-run_differential_expression_group <- function(sc_obj, analysis_dir, group) {
-  print(paste0("Performing differential expression for group ", group, " for each cell type"))
-  
-  # Make reading data parallel
-  if (Sys.getenv("SLURM_NTASKS_PER_NODE") == "") {
-    n.cores <- as.numeric(detectCores())
-  } else {
-    n.cores <- as.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE"))
-  }
-  
-  n.cores <- 16
-  
-  message(paste0("Number of cores: ", n.cores))
-  
-  registerDoMC(n.cores)
-  message("Begin parallelizing...")
-  
-  all_cell_types <- union(unique(sc_obj$predicted_celltype_majority_vote), unique(sc_obj$magical_cell_types))
-  
-  compiled_output <- foreach(
-    i = 1:length(all_cell_types),
-    .packages = c("Seurat", "base")
-  ) %dopar% {
-    cell_type <- all_cell_types[[i]]
-    print(cell_type)
-    if(cell_type %in% unique(sc_obj$predicted_celltype_majority_vote)) {
-      idxPass <- which(sc_obj$predicted_celltype_majority_vote %in% cell_type)
-    } else {
-      idxPass <- which(sc_obj$magical_cell_types %in% cell_type)
-      print("This cell type is for MAGICAL processing")
-    }
-    print(length(idxPass))
-    cellsPass <- names(sc_obj$orig.ident[idxPass])
-    cells_subset <- subset(x = sc_obj, subset = cell_name %in% cellsPass)
-    # TODO: Make this print table for relevant group
-    #print(table(cells_subset$viral_load))
-    DefaultAssay(cells_subset) <- "SCT"
-    Idents(cells_subset) <- group
-    if(group == "viral_load") {
-      first_group <- "high"
-      second_group <- "low"
-    } else if(group == "time_point") {
-      first_group <- "D28"
-      second_group <- "D_minus_1"
-    } else if(group == "sex") {
-      first_group <- "M"
-      second_group <- "F"
-    }
-    #diff_markers = FindMarkers(cells_subset, test.use="LR", latent.vars = 'subject_id', ident.1 = first_group, ident.2 = second_group, logfc.threshold = 0, min.pct = 0, assay = "SCT", recorrect_umi = FALSE)
-    diff_markers <- FindMarkers(cells_subset, ident.1 = first_group, ident.2 = second_group, logfc.threshold = 0.1, min.pct = 0.1, assay = "SCT", recorrect_umi = FALSE)
-    cell_type <- sub(" ", "_", cell_type)
-    write.csv(diff_markers, paste0(analysis_dir, first_group, "-vs-", second_group, "-degs-", cell_type, "-", group, ".csv"), quote = FALSE)
-    return(i)
-  }
-  print(paste0("Done performing differential expression for group ", group, " for each cell type"))
-}
-
 run_differential_expression_controlling_for_subject_id <- function(sc_obj, analysis_dir, sample_metadata_for_SPEEDI_df, group, cell_types) {
   print(paste0("Performing differential expression for group ", group, " for each cell type (controlling for subject ID)"))
   final_current_de <- data.frame(Cell_Type = character(), Gene_Name = character(), sc_pval_adj = character(), sc_log2FC = character(), pseudo_bulk_pval = character(),
