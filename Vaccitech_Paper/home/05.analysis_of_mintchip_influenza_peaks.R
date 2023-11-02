b68b57fb3410dd84b4b2d3967e2739601236dabb
William Thistlethwaite <williamthistle@gmail.com>
2023-11-01 22:05:45 -0400

    WT: Updates

--- Vaccitech_Paper/home/05.analysis_of_mintchip_influenza_peaks.R
+++ Vaccitech_Paper/home/05.analysis_of_mintchip_influenza_peaks.R
@@ -46,6 +46,10 @@ for(marker in mintchip_markers) {
 pos_mintchip_marker_das_list_annotated <- list()
 neg_mintchip_marker_das_list_annotated <- list()
 
+fc_thresholds <- c("0", "0.1", "0.2", "0.3", "0.585", "1", "2")
+
+homer_dir <- paste0(mintchip_das_dir, "HOMER/")
+
 for(marker in mintchip_markers) {
   old_das <- mintchip_marker_das_list[[marker]]
   current_pos_annotated_das <- list()
@@ -55,20 +59,54 @@ for(marker in mintchip_markers) {
     if(nrow(current_das) > 0) {
       pos_das <- current_das[current_das$log2FoldChange > 0,]
       if(nrow(pos_das) > 0) {
+        # Print HOMER friendly file
+        homer_df <- data.frame(peak_id = rownames(pos_das), chr = pos_das$chr, start = pos_das$start, end = pos_das$end, strand = "+")
+        current_fc_threshold <- fc_thresholds[index]
+        if(current_fc_threshold == "0") {
+          homer_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_homer_pos.tsv")
+        } else {
+          homer_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_", current_fc_threshold, "_homer_pos.tsv")
+        }
+        write.table(homer_df, file = homer_file_path, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+        # Annotate peaks and print to file
         pos_das <- pos_das[,c(1,2,3,4,6)]
         pos_das$length <- pos_das$end - pos_das$start
+
         pos_das <- annotatePeak(makeGRangesFromDataFrame(pos_das), TxDb = txdb, annoDb = "org.Hs.eg.db")
         pos_das <- as.data.frame(pos_das)
+        if(current_fc_threshold == "0") {
+          annotated_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_annotated_pos.tsv")
+        } else {
+          annotated_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_", current_fc_threshold, "_annotated_pos.tsv")
+        }
+        write.table(pos_das, file = annotated_file_path, quote = FALSE, sep = "\t", row.names = FALSE)
         current_pos_annotated_das[[index]] <- pos_das
       } else {
         current_pos_annotated_das[[index]] <- "EMPTY"
       }
       neg_das <- current_das[current_das$log2FoldChange < 0,]
       if(nrow(neg_das) > 0) {
+        # Print HOMER friendly file
+        homer_df <- data.frame(peak_id = rownames(neg_das), chr = neg_das$chr, start = neg_das$start, end = neg_das$end, strand = "+")
+        current_fc_threshold <- fc_thresholds[index]
+        if(current_fc_threshold == "0") {
+          homer_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_homer_neg.tsv")
+        } else {
+          homer_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_", current_fc_threshold, "_homer_neg.tsv")
+        }
+        write.table(homer_df, file = homer_file_path, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+        # Annotate peaks and print to file
         neg_das <- neg_das[,c(1,2,3,4,6)]
         neg_das$length <- neg_das$end - neg_das$start
         neg_das <- annotatePeak(makeGRangesFromDataFrame(neg_das), TxDb = txdb, annoDb = "org.Hs.eg.db")
         neg_das <- as.data.frame(neg_das)
+        current_fc_threshold <- fc_thresholds[index]
+        if(current_fc_threshold == "0") {
+          annotated_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_annotated_neg.tsv")
+        } else {
+          annotated_file_path <- paste0(mintchip_das_dir, marker, "_D28_D1_diff_filtered_", current_fc_threshold, "_annotated_neg.tsv")
+        }
+        write.table(neg_das, file = annotated_file_path, quote = FALSE, sep = "\t", row.names = FALSE)
         current_neg_annotated_das[[index]] <- neg_das
       } else {
         current_neg_annotated_das[[index]] <- "EMPTY"
