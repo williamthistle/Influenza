c38749dd5d69382ee5f9bba63d333b857a1f99b2
William Thistlethwaite <williamthistle@gmail.com>
2023-11-08 12:44:43 -0500

    Update 02.find_overlap_between_single_cell_degs_and_bulk_rna.R

--- Vaccitech_Paper/home/02.find_overlap_between_single_cell_degs_and_bulk_rna.R
+++ Vaccitech_Paper/home/02.find_overlap_between_single_cell_degs_and_bulk_rna.R
@@ -5,18 +5,22 @@ source(paste0(base_dir, "00.setup.R"))
 # Fill out HVL matrices (upregulated and downregulated genes, with alpha = 0.05 and 0.1)
 hvl_upregulated_sc_genes_in_bulk_0.05 <- fill_in_sc_deg_info_for_time_series(innate_sc_pseudobulk_deg_table, high_placebo_counts, high_placebo_metadata,
                                                              paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/"), "up", alpha = 0.05)
+saveRDS(hvl_upregulated_sc_genes_in_bulk_0.05, file = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_0.05.RDS"))
 hvl_downregulated_sc_genes_in_bulk_0.05 <- fill_in_sc_deg_info_for_time_series(innate_sc_pseudobulk_deg_table, high_placebo_counts, high_placebo_metadata,
                                                              paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/"), "down", alpha = 0.05)
-
+saveRDS(hvl_downregulated_sc_genes_in_bulk_0.05, file = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_0.05.RDS"))
 hvl_upregulated_sc_genes_in_bulk_0.1 <- fill_in_sc_deg_info_for_time_series(innate_sc_pseudobulk_deg_table, high_placebo_counts, high_placebo_metadata,
                                                                      paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/"), "up", alpha = 0.1)
+saveRDS(hvl_upregulated_sc_genes_in_bulk_0.1, file = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_0.1.RDS"))
 hvl_downregulated_sc_genes_in_bulk_0.1 <- fill_in_sc_deg_info_for_time_series(innate_sc_pseudobulk_deg_table, high_placebo_counts, high_placebo_metadata,
                                                                      paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/"), "down", alpha = 0.1)
+saveRDS(hvl_downregulated_sc_genes_in_bulk_0.1, file = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_0.1.RDS"))
 
 # HVL - upregulated, 0.05 alpha
 write.table(hvl_upregulated_sc_genes_in_bulk_0.05, file = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.05.tsv"), sep = "\t", quote = FALSE, row.names = FALSE)
 high_passing_upregulated_genes <- unique(hvl_upregulated_sc_genes_in_bulk_0.05$Gene)
 
+# Plot with D2/D5/D8/D28 in order
 hvl_upregulated_sc_genes_in_bulk_0.05_plot <- ggplot(data = hvl_upregulated_sc_genes_in_bulk_0.05, aes(x = Day, y = Gene, size = Fold.Change.Abs, color = Fold.Change.Direction)) +
   geom_point() +
   scale_color_manual(values = c("Positive" = "#00BFC4", "Not Significant" = "grey")) +
@@ -28,9 +32,31 @@ hvl_upregulated_sc_genes_in_bulk_0.05_plot <- ggplot(data = hvl_upregulated_sc_g
     size = "Fold Change (Absolute Value)",
     color = "Fold Change Direction"
   ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
   theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
 
 ggsave(filename = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.05.tiff"), plot = hvl_upregulated_sc_genes_in_bulk_0.05_plot, device='tiff', dpi=300)
+
+# Alternatively, plot with D28/D2/D5/D8 (can draw a line between D28 and the rest of the days)
+hvl_upregulated_sc_genes_in_bulk_0.05_alt <- hvl_upregulated_sc_genes_in_bulk_0.05
+hvl_upregulated_sc_genes_in_bulk_0.05_alt$Day <- factor(hvl_upregulated_sc_genes_in_bulk_0.05_alt$Day, levels = c("Day.28","Day.2","Day.5","Day.8"))
+hvl_upregulated_sc_genes_in_bulk_0.05_alt_plot <- ggplot(data = hvl_upregulated_sc_genes_in_bulk_0.05_alt, aes(x = Day, y = Gene, size = Fold.Change.Abs, color = Fold.Change.Direction)) +
+  geom_point() +
+  scale_color_manual(values = c("Positive" = "#00BFC4", "Not Significant" = "grey")) +
+  theme_minimal() +
+  labs(
+    title = "Fold Change of Upregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    x = "Day (Post Exposure)",
+    y = "Gene",
+    size = "Fold Change (Absolute Value)",
+    color = "Fold Change Direction"
+  ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
+  theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
+
+ggsave(filename = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.05_alt.tiff"), plot = hvl_upregulated_sc_genes_in_bulk_0.05_alt_plot, device='tiff', width = 12, height = 15)
 
 # HVL - downregulated, 0.05 alpha
 write.table(hvl_downregulated_sc_genes_in_bulk_0.05, file = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.05.tsv"), sep = "\t", quote = FALSE, row.names = FALSE)
@@ -41,17 +67,37 @@ hvl_downregulated_sc_genes_in_bulk_0.05_plot <- ggplot(data = hvl_downregulated_
   scale_color_manual(values = c("Positive" = "#00BFC4", "Negative" = "#F3756D", "Not Significant" = "grey")) +
   theme_minimal() +
   labs(
-    title = "Fold Change of downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    title = "Fold Change of Downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
     x = "Day (Post Exposure)",
     y = "Gene",
     size = "Fold Change (Absolute Value)",
     color = "Fold Change Direction"
   ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
   theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
 
 ggsave(filename = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.05.tiff"), plot = hvl_downregulated_sc_genes_in_bulk_0.05_plot, device='tiff', width = 12, height = 15)
 
-### I think I like 0.05 alpha better ###
+# Alternatively, plot with D28/D2/D5/D8 (can draw a line between D28 and the rest of the days)
+hvl_downregulated_sc_genes_in_bulk_0.05_alt <- hvl_downregulated_sc_genes_in_bulk_0.05
+hvl_downregulated_sc_genes_in_bulk_0.05_alt$Day <- factor(hvl_downregulated_sc_genes_in_bulk_0.05_alt$Day, levels = c("Day.28","Day.2","Day.5","Day.8"))
+hvl_downregulated_sc_genes_in_bulk_0.05_alt_plot <- ggplot(data = hvl_downregulated_sc_genes_in_bulk_0.05_alt, aes(x = Day, y = Gene, size = Fold.Change.Abs, color = Fold.Change.Direction)) +
+  geom_point() +
+  scale_color_manual(values = c("Positive" = "#00BFC4", "Negative" = "#F3756D", "Not Significant" = "grey")) +
+  theme_minimal() +
+  labs(
+    title = "Fold Change of Downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    x = "Day (Post Exposure)",
+    y = "Gene",
+    size = "Fold Change (Absolute Value)",
+    color = "Fold Change Direction"
+  ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
+  theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
+
+ggsave(filename = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.05_alt.tiff"), plot = hvl_downregulated_sc_genes_in_bulk_0.05_alt_plot, device='tiff', width = 12, height = 15)
 
 # HVL - upregulated, 0.1 alpha
 write.table(hvl_upregulated_sc_genes_in_bulk_0.1, file = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.1.tsv"), sep = "\t", quote = FALSE, row.names = FALSE)
@@ -68,9 +114,31 @@ hvl_upregulated_sc_genes_in_bulk_0.1_plot <- ggplot(data = hvl_upregulated_sc_ge
     size = "Fold Change (Absolute Value)",
     color = "Fold Change Direction"
   ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
   theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
 
-ggsave(filename = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.1.tiff"), plot = hvl_upregulated_sc_genes_in_bulk_0.1_plot, device='tiff', dpi=300)
+ggsave(filename = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.1.tiff"), plot = hvl_upregulated_sc_genes_in_bulk_0.1_plot, device='tiff', width = 12, height = 15)
+
+# Alternatively, plot with D28/D2/D5/D8 (can draw a line between D28 and the rest of the days)
+hvl_upregulated_sc_genes_in_bulk_0.1_alt <- hvl_upregulated_sc_genes_in_bulk_0.1
+hvl_upregulated_sc_genes_in_bulk_0.1_alt$Day <- factor(hvl_upregulated_sc_genes_in_bulk_0.1_alt$Day, levels = c("Day.28","Day.2","Day.5","Day.8"))
+hvl_upregulated_sc_genes_in_bulk_0.1_alt_plot <- ggplot(data = hvl_upregulated_sc_genes_in_bulk_0.1_alt, aes(x = Day, y = Gene, size = Fold.Change.Abs, color = Fold.Change.Direction)) +
+  geom_point() +
+  scale_color_manual(values = c("Positive" = "#00BFC4", "Not Significant" = "grey")) +
+  theme_minimal() +
+  labs(
+    title = "Fold Change of Upregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    x = "Day (Post Exposure)",
+    y = "Gene",
+    size = "Fold Change (Absolute Value)",
+    color = "Fold Change Direction"
+  ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
+  theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
+
+ggsave(filename = paste0(bulk_results_dir, "hvl_upregulated_sc_genes_found_in_bulk/hvl_upregulated_sc_genes_in_bulk_alpha_0.1_alt.tiff"), plot = hvl_upregulated_sc_genes_in_bulk_0.1_alt_plot, device='tiff', width = 12, height = 15)
 
 # HVL - downregulated, 0.1 alpha
 write.table(hvl_downregulated_sc_genes_in_bulk_0.1, file = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.1.tsv"), sep = "\t", quote = FALSE, row.names = FALSE)
@@ -81,16 +149,37 @@ hvl_downregulated_sc_genes_in_bulk_0.1_plot <- ggplot(data = hvl_downregulated_s
   scale_color_manual(values = c("Positive" = "#00BFC4", "Negative" = "#F3756D", "Not Significant" = "grey")) +
   theme_minimal() +
   labs(
-    title = "Fold Change of downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    title = "Fold Change of Downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
     x = "Day (Post Exposure)",
     y = "Gene",
     size = "Fold Change (Absolute Value)",
     color = "Fold Change Direction"
   ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
   theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
 
 ggsave(filename = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.1.tiff"), plot = hvl_downregulated_sc_genes_in_bulk_0.1_plot, device='tiff', width = 12, height = 15)
 
+# Alternatively, plot with D28/D2/D5/D8 (can draw a line between D28 and the rest of the days)
+hvl_downregulated_sc_genes_in_bulk_0.1_alt <- hvl_downregulated_sc_genes_in_bulk_0.1
+hvl_downregulated_sc_genes_in_bulk_0.1_alt$Day <- factor(hvl_downregulated_sc_genes_in_bulk_0.1_alt$Day, levels = c("Day.28","Day.2","Day.5","Day.8"))
+hvl_downregulated_sc_genes_in_bulk_0.1_alt_plot <- ggplot(data = hvl_downregulated_sc_genes_in_bulk_0.1_alt, aes(x = Day, y = Gene, size = Fold.Change.Abs, color = Fold.Change.Direction)) +
+  geom_point() +
+  scale_color_manual(values = c("Positive" = "#00BFC4", "Negative" = "#F3756D", "Not Significant" = "grey")) +
+  theme_minimal() +
+  labs(
+    title = "Fold Change of Downregulated Genes (at Day 28) from Innate Immune Cells Across Course of Infection",
+    x = "Day (Post Exposure)",
+    y = "Gene",
+    size = "Fold Change (Absolute Value)",
+    color = "Fold Change Direction"
+  ) +
+  guides(color  = guide_legend(order = 1),
+         size = guide_legend(order = 2)) +
+  theme(plot.title = element_text(hjust = 0.6)) + theme(aspect.ratio = 2/1)
+
+ggsave(filename = paste0(bulk_results_dir, "hvl_downregulated_sc_genes_found_in_bulk/hvl_downregulated_sc_genes_in_bulk_alpha_0.1_alt.tiff"), plot = hvl_downregulated_sc_genes_in_bulk_0.1_alt_plot, device='tiff', width = 12, height = 15)
 
 
 
