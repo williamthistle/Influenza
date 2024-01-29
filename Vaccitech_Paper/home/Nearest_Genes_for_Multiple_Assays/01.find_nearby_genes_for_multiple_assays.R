86bfc66217fdea06c6f0adb33f89ee719308890e
William Thistlethwaite <williamthistle@gmail.com>
2024-01-29 15:25:40 -0500

    Update 01.find_nearby_genes_for_multiple_assays.R

--- Vaccitech_Paper/home/Nearest_Genes_for_Multiple_Assays/01.find_nearby_genes_for_multiple_assays.R
+++ Vaccitech_Paper/home/Nearest_Genes_for_Multiple_Assays/01.find_nearby_genes_for_multiple_assays.R
@@ -3,6 +3,6 @@ base_dir <- "~/GitHub/Influenza/Vaccitech_Paper/home/"
 source(paste0(base_dir, "00.setup.R"))
 magical_results <- read.table(file = paste0(magical_output_dir, "MAGICAL_overall_output.tsv"), sep = "\t", header = TRUE)
 
-# Find snME within promoter regions of genes for each cell type
+
 snME_cell_types <- c("B-Mem", "B-Naive", "Monocyte", "NK-cell2", "Tc-Mem", "Tc-Naive", "Th-Mem", "Th-Naive")
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
