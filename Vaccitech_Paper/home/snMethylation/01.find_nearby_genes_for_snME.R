25eae21ce8f84821135471c34c7750eaf5a0c588
William Thistlethwaite <williamthistle@gmail.com>
2024-01-30 12:28:33 -0500

    Update 01.find_nearby_genes_for_snME.R

--- Vaccitech_Paper/home/snMethylation/01.find_nearby_genes_for_snME.R
+++ Vaccitech_Paper/home/snMethylation/01.find_nearby_genes_for_snME.R
@@ -3,10 +3,20 @@ base_dir <- "~/GitHub/Influenza/Vaccitech_Paper/home/"
 source(paste0(base_dir, "00.setup.R"))
 magical_results <- read.table(file = paste0(magical_output_dir, "MAGICAL_overall_output.tsv"), sep = "\t", header = TRUE)
 
+run_fmd_on_snME <- function(gene_list) {
+  if(length(gene_list) > 1 & length(gene_list) < 2000) {
+    current_fmd_result <- SPEEDI::RunFMD_RNA(gene_list, "blood")
+  } else { 
+    current_fmd_result <- "EMPTY OR OVER 2000 PEAKS (TOO MANY)"
+  }
+  return(current_fmd_result)
+}
+
 # Find snME within promoter regions of genes for each cell type
 snME_cell_types <- c("B-Mem", "B-Naive", "Monocyte", "NK-cell2", "Tc-Mem", "Tc-Naive", "Th-Mem", "Th-Naive")
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 
+# Annotate genes for methylated promoters
 for(snME_cell_type in snME_cell_types) {
   snME_dms_for_current_cell_type <- snME_dms[snME_dms$celltype == snME_cell_type,]
   upregulated_snME_dms_for_current_cell_type <- snME_dms_for_current_cell_type[snME_dms_for_current_cell_type$methylation == "upregulated",]
@@ -25,4 +35,16 @@ for(snME_cell_type in snME_cell_types) {
   write.table(downregulated_differential_analysis_results_df, file = paste0(snME_results_dir, "Closest_Gene_Analysis/", snME_cell_type, "_D_minus_1_hypermethylated_annotated_genes.tsv"), 
               sep = "\t", quote = FALSE)
 }
+
+# Run HB on positive (hypermethylated in D28) genes
+pos_fmd_snME_list <- list()
+for(snME_cell_type in snME_cell_types) {
+  pos_snME_results_file <- paste0(snME_results_dir, "Closest_Gene_Analysis/", snME_cell_type, "_D28_hypermethylated_annotated_genes.tsv")
+  pos_snME_cell_type_annotations <- read.table(pos_snME_results_file, sep = "\t", header = TRUE, comment.char = "", quote = "\"")
+  pos_genes <- unique(pos_snME_cell_type_annotations$SYMBOL)
+  pos_results <- run_fmd_on_mintchip(pos_genes)
+  pos_fmd_list[[marker]][[as.character(fc)]] <- pos_results
+}
+
+
 
