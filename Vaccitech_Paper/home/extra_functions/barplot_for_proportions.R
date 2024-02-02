44764a3d2161afdf3f4f4d4566a37489ef3d1a1d
William Thistlethwaite <williamthistle@gmail.com>
2024-02-02 10:42:55 -0500

    Update barplot_for_proportions.R

--- Vaccitech_Paper/home/extra_functions/barplot_for_proportions.R
+++ Vaccitech_Paper/home/extra_functions/barplot_for_proportions.R
@@ -1,19 +1,19 @@
 # RNA
-test <- read.table("C:/Users/willi/Desktop/RNA_cell_type_proportion_time_point.csv", sep = ",", header = TRUE)
+test <- read.table("C:/Users/wat2/Desktop/RNA_cell_type_proportion_time_point.csv", sep = ",", header = TRUE)
 
-#manova_results <- manova(cbind(CD16_Mono, T_Naive, CD14_Mono, CD4_Memory, Platelet, B, cDC, NK_MAGICAL, MAIT, CD8_Memory, pDC, Proliferating, HSPC) ~ Condition, data = test)
+manova_results <- manova(cbind(CD16_Mono, T_Naive, CD14_Mono, CD4_Memory, Platelet, B, cDC, NK_MAGICAL, MAIT, CD8_Memory, pDC, Proliferating, HSPC) ~ Condition, data = test)
 #one.way <- aov(CD16_Mono ~ Condition, data = test)
 #one.way <- aov(CD14_Mono ~ Condition, data = test)
 #one.way <- aov(NK_MAGICAL ~ Condition, data = test)
 
-# Only CD14 Mono has any significance
-kruskal.test(CD14_Mono ~ Condition, data = test)
+# Only CD14 Mono has any significance (HVL) - no significance otherwise
+kruskal.test(CD4_Memory ~ Condition, data = test)
 
 # Get ready for stacked barplot
 results <- aggregate(cbind(CD16_Mono, T_Naive, CD14_Mono, CD4_Memory, Platelet, B, cDC, NK_MAGICAL, MAIT, CD8_Memory, pDC, Proliferating, HSPC) ~ Condition, data = test, sum)
 for(colname in colnames(results)) {
   if(colname != "Condition") {
-    results[[colname]] <- results[[colname]] / 4
+    results[[colname]] <- results[[colname]] / 6
   }
 }
 
@@ -34,12 +34,12 @@ results_long <- results_long %>%
 rna_barplot <- ggplot(results_long, aes(fill=Cell_Type, y=Expression, x=Condition)) + 
   geom_bar(position="stack", stat="identity") + 
   xlab("Group") + ylab("Cell Type Proportion") + scale_fill_brewer(palette = "Paired") +
-  labs(fill = "Cell Type") +
-  ggtitle("Cell Type Proportions for scRNA-Seq (High Viral Load)") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(fill = "Cell Type") + theme_bw() +
+  ggtitle("Cell Type Proportions for scRNA-Seq") + theme(plot.title = element_text(hjust = 0.5)) +
   theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + coord_fixed(ratio = 1.5) + theme_bw()
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + coord_fixed(ratio = 1.5)
 
-ggsave("C:/Users/willi/Desktop/rna_barplot.png", plot = rna_barplot, device = "tiff", dpi = 300)
+ggsave("C:/Users/wat2/Desktop/rna_barplot.tiff", plot = rna_barplot, device = "tiff", dpi = 300)
 
 
 
@@ -47,7 +47,7 @@ ggsave("C:/Users/willi/Desktop/rna_barplot.png", plot = rna_barplot, device = "t
 
 
 # ATAC
-test <- read.table("C:/Users/willi/Desktop/ATAC_cell_type_proportion_time_point.csv", sep = ",", header = TRUE)
+test <- read.table("C:/Users/wat2/Desktop/ATAC_cell_type_proportion_time_point.csv", sep = ",", header = TRUE)
 
 #manova_results <- manova(cbind(T.Naive, CD8.Memory, CD4.Memory, B, NK, CD14.Mono, MAIT, CD16.Mono, Proliferating) ~ Condition, data = test)
 #one.way <- aov(CD16_Mono ~ Condition, data = test)
@@ -55,13 +55,13 @@ test <- read.table("C:/Users/willi/Desktop/ATAC_cell_type_proportion_time_point.
 #one.way <- aov(NK_MAGICAL ~ Condition, data = test)
 
 # Nothing has any significance
-kruskal.test(B ~ Condition, data = test)
+kruskal.test(CD14.Mono ~ Condition, data = test)
 
 # Get ready for stacked barplot
 results_ATAC <- aggregate(cbind(T.Naive, CD8.Memory, CD4.Memory, B, NK, CD14.Mono, MAIT, CD16.Mono, Proliferating) ~ Condition, data = test, sum)
 for(colname in colnames(results_ATAC)) {
   if(colname != "Condition") {
-    results_ATAC[[colname]] <- results_ATAC[[colname]] / 4
+    results_ATAC[[colname]] <- results_ATAC[[colname]] / 6
   }
 }
 
@@ -80,9 +80,9 @@ results_ATAC_long <- results_ATAC_long %>%
 atac_barplot <- ggplot(results_ATAC_long, aes(fill=Cell_Type, y=Expression, x=Condition)) + 
   geom_bar(position="stack", stat="identity") + 
   xlab("Group") + ylab("Cell Type Proportion") + scale_fill_brewer(palette = "Paired") +
-  labs(fill = "Cell Type") +
-  ggtitle("Cell Type Proportions for scATAC-Seq (High Viral Load)") + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(fill = "Cell Type") + theme_bw() +
+  ggtitle("Cell Type Proportions for scATAC-Seq") + theme(plot.title = element_text(hjust = 0.5)) +
   theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + coord_fixed(ratio = 1.5) + theme_bw()
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + coord_fixed(ratio = 1.5) 
 
-ggsave("C:/Users/willi/Desktop/atac_barplot.png", plot = atac_barplot, device = "tiff", dpi = 300)
+ggsave("C:/Users/wat2/Desktop/atac_barplot.tiff", plot = atac_barplot, device = "tiff", dpi = 300)
