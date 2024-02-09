ad2c664205467631e9ec0d97d5dad045c1cee3ea
William Thistlethwaite <williamthistle@gmail.com>
2024-02-09 07:50:35 -0500

    Update 14.scRNA_humanbase_analyses_innate_full_sc.R

--- Vaccitech_Paper/home/scRNA-Seq/14.scRNA_humanbase_analyses_innate_full_sc.R
+++ Vaccitech_Paper/home/scRNA-Seq/14.scRNA_humanbase_analyses_innate_full_sc.R
@@ -33,3 +33,25 @@ neg_innate_genes <- unique(neg_innate_genes)
 
 pos_innate_fmd <- run_fmd_on_scRNA(pos_innate_genes)
 neg_innate_fmd <- run_fmd_on_scRNA(neg_innate_genes)
+
+
+pos_adaptive_genes <- c()
+neg_adaptive_genes <- c()
+
+for(adaptive_cell_type in adaptive_cell_types) {
+  adaptive_cell_type_for_file_name <- sub(" ", "_", adaptive_cell_type)
+  current_deg_table <- read.table(paste0(sc_deg_dir, "D28-vs-D_minus_1-degs-", adaptive_cell_type_for_file_name, "-time_point-controlling_for_subject_id_sc.tsv"), sep = "\t", header = TRUE)
+  pos_deg_table <- current_deg_table[current_deg_table$avg_log2FC > 0,]
+  neg_deg_table <- current_deg_table[current_deg_table$avg_log2FC < 0,]
+  pos_adaptive_genes <- c(pos_adaptive_genes, rownames(pos_deg_table))
+  neg_adaptive_genes <- c(neg_adaptive_genes, rownames(neg_deg_table))
+}
+
+pos_adaptive_genes <- unique(pos_adaptive_genes)
+neg_adaptive_genes <- unique(neg_adaptive_genes)
+
+pos_adaptive_fmd <- run_fmd_on_scRNA(pos_adaptive_genes)
+neg_adaptive_fmd <- run_fmd_on_scRNA(neg_adaptive_genes)
+
+
+
