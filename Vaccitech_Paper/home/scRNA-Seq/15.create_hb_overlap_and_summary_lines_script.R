457a6f6c120e2ee9f28c34ec952780b2eb2f03d6
William Thistlethwaite <williamthistle@gmail.com>
2024-02-15 12:34:35 -0500

    WT: Updates

--- Vaccitech_Paper/home/scRNA-Seq/15.create_hb_overlap_and_summary_lines_script.R
+++ Vaccitech_Paper/home/scRNA-Seq/15.create_hb_overlap_and_summary_lines_script.R
@@ -25,7 +25,11 @@ fc_direction <- "downregulated"
 
 get_lists_of_commands(analysis_dir, fc_direction)
 
+# UPREGULATED snME
+analysis_dir <- "C:/Users/wat2/OneDrive - Princeton University/Vaccitech_Paper/network_comparisons/snME_upregulated_vs_scRNA_downregulated_comparisons/"
+fc_direction <- "upregulated"
 
+get_lists_of_commands(analysis_dir, fc_direction, "snME")
 
 
 
