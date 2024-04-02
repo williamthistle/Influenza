5219bdaed15d4c6ef87e84e2c4caad70f8835b23
William Thistlethwaite <williamthistle@gmail.com>
2024-04-02 16:30:32 -0400

    Update 21.find_overlapping_hvl_and_lvl_degs.R

--- Vaccitech_Paper/home/scRNA-Seq/21.find_overlapping_hvl_and_lvl_degs.R
+++ Vaccitech_Paper/home/scRNA-Seq/21.find_overlapping_hvl_and_lvl_degs.R
@@ -1,9 +1,9 @@
-overlapping_cell_types <- intersect(unique(sc_pseudobulk_deg_table$Cell_Type), unique(lvl_sc_degs_table$Cell_Type))
+overlapping_cell_types <- intersect(unique(scRNA_hvl_placebo_degs$Cell_Type), unique(scRNA_lvl_placebo_degs$Cell_Type))
 
 for(cell_type in overlapping_cell_types) {
   print(cell_type)
-  hvl_subset <- sc_pseudobulk_deg_table[sc_pseudobulk_deg_table$Cell_Type == cell_type,]
-  lvl_subset <- lvl_sc_pseudobulk_deg_table[lvl_sc_pseudobulk_deg_table$Cell_Type == cell_type,]
+  hvl_subset <- scRNA_hvl_placebo_degs[scRNA_hvl_placebo_degs$Cell_Type == cell_type,]
+  lvl_subset <- scRNA_lvl_placebo_degs[scRNA_lvl_placebo_degs$Cell_Type == cell_type,]
   pos_hvl_subset <- hvl_subset[hvl_subset$sc_log2FC > 0,]
   pos_lvl_subset <- lvl_subset[lvl_subset$sc_log2FC > 0,]
   neg_hvl_subset <- hvl_subset[hvl_subset$sc_log2FC < 0,]
