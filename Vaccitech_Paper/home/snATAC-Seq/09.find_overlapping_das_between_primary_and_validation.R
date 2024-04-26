f27d8059d8aaf346918dca92e8b26583ea5b84de
William Thistlethwaite <williamthistle@gmail.com>
2024-04-26 12:45:28 -0400

    Create 09.find_overlapping_das_between_primary_and_validation.R

---
+++ Vaccitech_Paper/home/snATAC-Seq/09.find_overlapping_das_between_primary_and_validation.R
@@ -0,0 +1,25 @@
+# Setup environment
+base_dir <- "~/GitHub/Influenza/Vaccitech_Paper/home/"
+source(paste0(base_dir, "00.setup.R"))
+
+overlapping_das_cell_types <- c("CD14_Mono", "CD16_Mono", "cDC", "pDC", "NK", "CD4_Memory", "CD8_Memory", "CD8_Naive", "MAIT", "B", "Proliferating", "CD4_Naive")
+
+for(cell_type in overlapping_das_cell_types) {
+  for(analysis_type in c("sc", "final")) {
+    for(min_pct in c(0.01, 0.05, 0.1)) {
+      print(paste0("Overlap for ", cell_type, " (", analysis_type, ", ", min_pct, ")"))
+      placebo_das <- read.table(paste0(scATAC_hvl_placebo_das_dir, "D28-vs-D_minus_1-degs-", cell_type, "-time_point-controlling_for_subject_id_", analysis_type, "_pct_", min_pct, ".tsv"),
+                                     sep = "\t", header = TRUE)
+      vaccination_das <- read.table(paste0(scATAC_hvl_vaccinated_das_dir, "D28-vs-D_minus_1-degs-", cell_type, "-time_point-controlling_for_subject_id_", analysis_type, "_pct_", min_pct, ".tsv"),
+                                    sep = "\t", header = TRUE)
+      if(analysis_type == "sc") {
+        overlapping_das <- intersect(rownames(placebo_das), rownames(vaccination_das))
+      } else {
+        overlapping_das <- intersect(placebo_das$Peak_Name, vaccination_das$Peak_Name)
+      }
+      print(paste0("Total overlapping DAS: ", length(overlapping_das)))
+      print(paste0("Total placebo DAS: ", nrow(placebo_das)))
+      print(paste0("Percent of placebo DAS that overlap: ", (length(overlapping_das) / nrow(placebo_das) * 100)))
+    }
+  }
+}
