ee235318d56c86d2ea2c59a170113ac0a882e380
William Thistlethwaite <williamthistle@gmail.com>
2024-07-08 21:52:30 -0400

    Update 16.check_annotation_of_peak.R

--- Vaccitech_Paper/home/snATAC-Seq/16.check_annotation_of_peak.R
+++ Vaccitech_Paper/home/snATAC-Seq/16.check_annotation_of_peak.R
@@ -4,7 +4,7 @@ source(paste0(base_dir, "00.setup.R"))
 
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 # Peak to check
-checking_peak <- data.frame(seqnames = "chr3", start = "190634549", end = "190634949")
+checking_peak <- data.frame(seqnames = "chr11", start = "66069371", end = "66069871")
 checking_peak_annotated <- annotatePeak(makeGRangesFromDataFrame(checking_peak), TxDb = txdb, annoDb = "org.Hs.eg.db")
 checking_peak_annotated <- as.data.frame(checking_peak_annotated)
 checking_peak_annotated
