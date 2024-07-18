65411b75ffe1f831389929979c104d1395fc2418
William Thistlethwaite <williamthistle@gmail.com>
2024-07-18 14:17:05 -0400

    Update 32.annotate_genes.R

--- Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
+++ Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
@@ -23,7 +23,7 @@ annotations <- getBM(
   mart = ensembl
 )
 
-# ABHD17A, ASH1L, BRD1, CASP1, CSF1R, CSK, DNAJC3, DUSP1, DUSP2, DUSP6, ETS1, FOSB, FOSL2, GBP5, IFNG, IL1RAP, IRAK3, IRF2, ITGAL, JDP2, KDM2B, KDM3A, MAP3K11, MAP4K3, MAPK7, METTL23, MINK1, PSMB9, RIPK1, SETD2, STAT4
+# ABHD17A, ASH1L, BRD1, CASP1, CSF1R, CSK, DNAJC3, DUSP1, DUSP2, DUSP6, ETS1, FOSB, FOSL2, GBP5, IFNG, IL1RAP, IRAK3, IRF2, JDP2, KDM2B, KDM3A, MAP3K11, MAP4K3, MAPK7, METTL23, MINK1, PSMB9, RIPK1, SETD2, STAT4
 
 annotations_final <- annotations %>%
   filter(
@@ -34,3 +34,4 @@ annotations_final <- annotations %>%
 important_magical_genes <- c("ABHD17A", "ASH1L", "BRD1", "CASP1", "CSF1R", "CSK", "DNAJC3", "DUSP1", "DUSP2", "DUSP6", "ETS1", "FOSB", "FOSL2", "GBP5", "IFNG", "IL1RAP", "IRAK3", "IRF2", "ITGAL", "JDP2", "KDM2B", "KDM3A", "MAP3K11", "MAP4K3", "MAPK7", "METTL23", "MINK1", "PSMB9", "RIPK1", "SETD2", "STAT4")
 important_overall_magical_df <- overall_magical_df[overall_magical_df$Gene_symbol %in% important_magical_genes,]
 important_magical_gene_overlap_df <- magical_gene_overlap_df[magical_gene_overlap_df$Gene_Name %in% important_magical_genes,]
+write.table(important_magical_gene_overlap_df, file = "C:/Users/wat2/Desktop/important_magical_gene_overlap.tsv", sep = "\t", row.names = FALSE)
