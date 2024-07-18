1e0bc08b0e81f7d415df139a2fef0d3c5e18975b
William Thistlethwaite <williamthistle@gmail.com>
2024-07-18 13:36:15 -0400

    Update 32.annotate_genes.R

--- Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
+++ Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
@@ -13,3 +13,24 @@ annotations <- getBM(
 
 # Search for histone related genes
 annotations[grep("histone", annotations$description),]
+
+# annotate MAGICAL genes
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+annotations <- getBM(
+  attributes = c('hgnc_symbol', 'description', 'chromosome_name', 'start_position', 'end_position'),
+  filters = 'hgnc_symbol',
+  values = sort(unique(overall_magical_df$Gene_symbol)) ,
+  mart = ensembl
+)
+
+# ABHD17A, ASH1L, BRD1, CASP1, CSF1R, CSK, DNAJC3, DUSP1, DUSP2, DUSP6, ETS1, FOSB, FOSL2, GBP5, IFNG, IL1RAP, IRAK3, IRF2, ITGAL, JDP2, KDM2B, KDM3A, MAP3K11, MAP4K3, MAPK7, METTL23, MINK1, PSMB9, RIPK1, SETD2, STAT4
+
+annotations_final <- annotations %>%
+  filter(
+    str_detect(hgnc_symbol, paste(gene_terms, collapse = "|")) |
+      str_detect(description, paste(search_terms, collapse = "|"))
+  )
+
+important_magical_genes <- c("ABHD17A", "ASH1L", "BRD1", "CASP1", "CSF1R", "CSK", "DNAJC3", "DUSP1", "DUSP2", "DUSP6", "ETS1", "FOSB", "FOSL2", "GBP5", "IFNG", "IL1RAP", "IRAK3", "IRF2", "ITGAL", "JDP2", "KDM2B", "KDM3A", "MAP3K11", "MAP4K3", "MAPK7", "METTL23", "MINK1", "PSMB9", "RIPK1", "SETD2", "STAT4")
+important_overall_magical_df <- overall_magical_df[overall_magical_df$Gene_symbol %in% important_magical_genes,]
+important_magical_gene_overlap_df <- magical_gene_overlap_df[magical_gene_overlap_df$Gene_Name %in% important_magical_genes,]
