ecb73be8553128d4967519df06fdc160d0b6499a
William Thistlethwaite <williamthistle@gmail.com>
2024-07-19 16:54:55 -0400

    WT: Updates

--- Vaccitech_Paper/home/MAGICAL/04.create_table_for_paper.R
+++ Vaccitech_Paper/home/MAGICAL/04.create_table_for_paper.R
@@ -10,91 +10,9 @@ source(paste0(base_dir, "extra_functions/my_MAGICAL_helper_functions.R"))
 
 overall_magical_df <- read.table(paste0(MAGICAL_hvl_placebo_output_dir, "MAGICAL_overall_output_with_stage_1_info.tsv"), sep = "\t", header = TRUE)
 magical_gene_overlap_df <- create_magical_gene_overlap_df(overall_magical_df, hvl_full_time_series_placebo_period_2_D5_vs_D_minus_1_results, hvl_full_time_series_placebo_period_2_D8_vs_D_minus_1_results)
-magical_tf_vs_gene_df <- create_tf_targets_df(overall_magical_df)
 
-create_tf_barplots_for_magical_gene <- function(overall_magical_df, current_gene, current_cell_type) {
-  current_cell_type_for_file_name <- gsub(" ", "_", current_cell_type)
-  current_gene_magical_info <- overall_magical_df[overall_magical_df$Cell_Type == current_cell_type_for_file_name,]
-  current_gene_magical_info <- current_gene_magical_info[current_gene_magical_info$Gene_symbol == current_gene,]
-  site_plots <- list()
-  for(current_row_index in 1:nrow(current_gene_magical_info)) {
-    current_row <- current_gene_magical_info[current_row_index,]
-    # Parse out list of TFs
-    current_row <- current_row %>%
-      mutate(TFs.binding.prob. = strsplit(as.character(TFs.binding.prob.), ",")) %>%
-      unnest(TFs.binding.prob.) %>%
-      mutate(TFs.binding.prob. = trimws(TFs.binding.prob.)) %>%
-      filter(!is.na(TFs.binding.prob.))
-    current_row <- subset(current_row, !grepl("^\\(", TFs.binding.prob.))
-    list_of_current_tfs <- current_row$TFs.binding.prob.
-    gene_name_vector <- c()
-    fold_change_vector <- c()
-    significance_vector <- c()
-    current_scRNA_hvl_placebo_degs <- innate_scRNA_hvl_placebo_degs[innate_scRNA_hvl_placebo_degs$Cell_Type == current_cell_type,]
-    current_unfiltered_hvl_placebo_degs <- read.table(paste0(scRNA_hvl_placebo_deg_dir, "D28-vs-D_minus_1-degs-", current_cell_type_for_file_name, "-time_point-controlling_for_subject_id_sc_unfiltered.tsv"),
-                                                      sep = "\t", header = TRUE)
-    for(current_tf in list_of_current_tfs) {
-      current_gene_unfiltered_data <- current_unfiltered_hvl_placebo_degs[rownames(current_unfiltered_hvl_placebo_degs) == current_tf,]
-      gene_name_vector <- c(gene_name_vector, current_tf)
-      fold_change_vector <- c(fold_change_vector, current_gene_unfiltered_data$avg_log2FC)
-      current_p_value <- current_gene_unfiltered_data$p_val_adj
-      min.pct.threshold.met <- current_gene_unfiltered_data$pct.1 >= 0.1 | current_gene_unfiltered_data$pct.2 >= 0.1
-      if(current_p_value < 0.001 & min.pct.threshold.met) {
-        significance_vector <- c(significance_vector, "***")
-      } else if(current_p_value < 0.01 & min.pct.threshold.met) {
-        significance_vector <- c(significance_vector, "**")
-      } else if(current_p_value < 0.05 & min.pct.threshold.met) {
-        significance_vector <- c(significance_vector, "*")
-      } else {
-        significance_vector <- c(significance_vector, NA)
-      }
-    }
-    current_site_df <- data.frame(TF = gene_name_vector, Fold_Change = fold_change_vector, significant = significance_vector)
-    
-    current_site_df <- current_site_df[rev(order(current_site_df$Fold_Change)),]
-    current_site_df$TF <- factor(current_site_df$TF, levels = current_site_df$TF)
-    current_site_df$Fold_Change_Direction <- ifelse(current_site_df$Fold_Change > 0, "Positive", "Negative")
-    current_site_df$Fold_Change_Direction <- factor(current_site_df$Fold_Change_Direction, levels = c("Positive", "Negative"))
-    
-    current_plot <- ggplot(current_site_df, aes(x = TF, y = Fold_Change, fill = Fold_Change_Direction)) +
-      geom_col() + geom_text(aes(label = significant,
-                                 vjust = ifelse(Fold_Change >= 0, 0, 1)),
-                             size = 6) + theme_bw() + ylim(-1, 1) + theme(text = element_text(size = 16)) +
-      labs(title = paste0("Site ", current_row_index),
-           x = "TF Name",
-           y = "Fold Change", fill = "Fold Change Direction") + theme(plot.title = element_text(hjust = 0.5)) + theme(legend.position="none")
-    
-    site_plots[[current_row_index]] <- current_plot
-  }
-  return(site_plots)
-}
-
-# MAP3K11
-MAP3K11_plots <- create_tf_barplots_for_magical_gene(overall_magical_df, "MAP3K11", "CD14_Mono")
-ggsave("C:/Users/willi/Desktop/MAP3K11_TF_plots.png", plot = patchwork::wrap_plots(MAP3K11_plots, ncol = 2, nrow = 2), height = 10, width = 18)
-
-# FOSB
-FOSB_plots <- create_tf_barplots_for_magical_gene(overall_magical_df, "FOSB", "CD16_Mono")
-ggsave("C:/Users/willi/Desktop/FOSB_TF_plots.png", plot = patchwork::wrap_plots(FOSB_plots, ncol = 1, nrow = 1), height = 7, width = 9)
-
-# IL1RAP
-IL1RAP_plots <- create_tf_barplots_for_magical_gene(overall_magical_df, "IL1RAP", "CD14_Mono")
-ggsave("C:/Users/willi/Desktop/IL1RAP_TF_plots.png", plot = patchwork::wrap_plots(IL1RAP_plots, ncol = 1, nrow = 1), height = 7, width = 9)
-
-# ASH1L
-ASH1L_plots <- create_tf_barplots_for_magical_gene(overall_magical_df, "ASH1L", "cDC")
-ggsave("C:/Users/willi/Desktop/ASH1L_TF_plots.png", plot = patchwork::wrap_plots(ASH1L_plots, ncol = 2, nrow = 2), height = 7, width = 18)
-
-# SETD2 - CD14 Mono
-SETD2_plots_CD14_Mono <- create_tf_barplots_for_magical_gene(overall_magical_df, "SETD2", "CD14_Mono")
-ggsave("C:/Users/willi/Desktop/SETD2_TF_plots_CD14.png", plot = patchwork::wrap_plots(SETD2_plots_CD14_Mono, ncol = 2, nrow = 3), height = 12, width = 18)
-
-# SETD2 - cDC
-SETD2_plots_cDC <- create_tf_barplots_for_magical_gene(overall_magical_df, "SETD2", "cDC")
-ggsave("C:/Users/willi/Desktop/SETD2_TF_plots_cDC.png", plot = patchwork::wrap_plots(SETD2_plots_cDC, ncol = 1, nrow = 1), height = 7, width = 9)
-
-
-
-
-
+# Parse to important genes
+important_magical_genes <- c("IL32", "CASP1", "CSF1R", "IRAK3", "IL1RAP", "NFIL3", "PTGES", "IL34", "CSF1R", "IL1B", "IL15RA", "IL21R", "IL1RN", "IL6R", "IL6R", "IL5RA", "IL4R", "IL6", "IL37", "IL17F", "IL20RB", "IRAK4", "IL10RA", "IL17B", "IL11", "IRF2", "IRF7", "IFNG", "OAS1", "PSMB9", "MNDA", "DNAJC3", "IFNGR1", "GBP5", "USP38", "IRF2", "IRF1", "IFNGR1", "IFI35", "IRF8", "IFITM3", "IRF5", "IFI6", "IRF1", "STING1", "IRF5", "IFI27", "IFNAR2", "LUARIS", "STING1", "ABHD17A", "CSK", "MAP3K11", "DUSP1", "DUSP2", "DUSP6", "MAPK7", "MAPK8", "MAP2K1", "MAP3K8", "MAP3K20", "MAP4K3", "MAPKAPK2", "PTK2B", "RELL1", "MINK1", "BRAF", "RIPK1", "MAPK13", "MAP2K5", "MAP3K11", "DUSP16", "DUSP16", "MAP3K15", "MAPK3", "MAP3K11", "MAP4K4", "MAP3K6", "MAPKBP1", "MAP3K3", "MAPK8IP1", "MINK1", "MAP4K4", "MAP4K3", "RIPK1", "MAP2K3", "MAP4K4", "MAP4K1", "MAP2K6", "MAP2K3", "MAPK15", "MAP2K2", "MAP4K2", "MAP2K1", "MAPKAP1", "MAPK10", "MAP4K1", "FOSL2", "FOSL1", "JUND", "JUN", "JUNB", "JUND", "JDP2", "FOSL2", "FOS", "JAK1", "STAT3", "STAT4", "JAK1", "STAT2", "STAT4", "STAT6", "JAK2", "JAK3", "STAT4", "JAK2", "JAK2", "JAK1", "STAT5A", "CCL3", "CX3CR1", "CCL3L1", "CXCL16 ", "CXCR2", "CCL22", "CCL26", "CXCR3", "CCL17", "CCR7", "CXCL12", "XCR1", "HIST1H1C", "HIST1H1D", "HIST1H1E", "H2AFZ", "H2BC5", "H2BC18", "H4C13", "H2BC15", "H3C2", "H2AC14", "H2AC4", "H4C8", "H2AC13", "H3-3A", "H2BC10", "H4C5", "H3-3B", "H2AC25", "H1-4", "KAT6A", "BRD1", "MSL2", "KAT2B", "KAT2A", "KAT2B", "BRD1", "MSL2", "KAT5", "HDAC5", "HDAC7", "HDAC9", "HDAC10", "HDAC4", "HDAC4", "HDAC11", "METTL23", "ASH1L", "PRDM2", "SETD2", "ASH1L", "SUV39H1", "ASH2L", "KMT2B", "SETD7", "PRMT2", "KMT2C", "KDM3A", "KDM2B", "KDM2B", "KDM5B", "KDM2A", "KDM7A", "KDM2A", "KDM8", "KDM4A", "KDM4B", "KDM2B", "KDM6A", "KDM4C", "KDM4A", "KDM4B", "KDM4E", "KDM5A")
+important_overall_magical_df <- overall_magical_df[overall_magical_df$Gene_symbol %in% important_magical_genes,]
+important_magical_gene_overlap_df <- magical_gene_overlap_df[magical_gene_overlap_df$Gene_Name %in% important_magical_genes,]
  
--- Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
+++ Vaccitech_Paper/home/scRNA-Seq/32.annotate_genes.R
@@ -2,6 +2,9 @@
 base_dir <- "~/GitHub/Influenza/Vaccitech_Paper/home/"
 source(paste0(base_dir, "00.setup.R"))
 
+source(paste0(base_dir, "extra_functions/MAGICAL_functions.R"))
+source(paste0(base_dir, "extra_functions/my_MAGICAL_helper_functions.R"))
+
 innate_scRNA_hvl_placebo_degs <- scRNA_hvl_placebo_degs[scRNA_hvl_placebo_degs$Cell_Type %in% innate_cell_types,]
 ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 annotations <- getBM(
@@ -23,7 +26,13 @@ annotations <- getBM(
   mart = ensembl
 )
 
-# ABHD17A, ASH1L, BRD1, CASP1, CSF1R, CSK, DNAJC3, DUSP1, DUSP2, DUSP6, ETS1, FOSB, FOSL2, GBP5, IFNG, IL1RAP, IRAK3, IRF2, JDP2, KDM2B, KDM3A, MAP3K11, MAP4K3, MAPK7, METTL23, MINK1, PSMB9, RIPK1, SETD2, STAT4
+overall_magical_df <- read.table(paste0(MAGICAL_hvl_placebo_output_dir, "MAGICAL_overall_output_with_stage_1_info.tsv"), sep = "\t", header = TRUE)
+magical_gene_overlap_df <- create_magical_gene_overlap_df(overall_magical_df, hvl_full_time_series_placebo_period_2_D5_vs_D_minus_1_results, hvl_full_time_series_placebo_period_2_D8_vs_D_minus_1_results)
+#magical_site_overlap_df <- create_magical_site_overlap_df(overall_magical_df)
+
+
+
+# ABHD17A, ASH1L, BRD1, CASP1, CSF1R, CSK, DNAJC3, DUSP1, DUSP2, DUSP6, ETS1, FOSB, FOSL2, GBP5, IFNG, IL1RAP, IRAK3, IRF2, JDP2, KDM2B, KDM3A, MAP3K11, MAP4K3, MAPK7, METTL23, MINK1, NFIL3, PSMB9, RIPK1, SETD2, STAT4
 
 annotations_final <- annotations %>%
   filter(
@@ -31,7 +40,7 @@ annotations_final <- annotations %>%
       str_detect(description, paste(search_terms, collapse = "|"))
   )
 
-important_magical_genes <- c("ABHD17A", "ASH1L", "BRD1", "CASP1", "CSF1R", "CSK", "DNAJC3", "DUSP1", "DUSP2", "DUSP6", "ETS1", "FOSB", "FOSL2", "GBP5", "IFNG", "IL1RAP", "IRAK3", "IRF2", "ITGAL", "JDP2", "KDM2B", "KDM3A", "MAP3K11", "MAP4K3", "MAPK7", "METTL23", "MINK1", "PSMB9", "RIPK1", "SETD2", "STAT4")
+important_magical_genes <- c("IL32", "CASP1", "CSF1R", "IRAK3", "IL1RAP", "NFIL3", "PTGES", "IL34", "CSF1R", "IL1B", "IL15RA", "IL21R", "IL1RN", "IL6R", "IL6R", "IL5RA", "IL4R", "IL6", "IL37", "IL17F", "IL20RB", "IRAK4", "IL10RA", "IL17B", "IL11", "IRF2", "IRF7", "IFNG", "OAS1", "PSMB9", "MNDA", "DNAJC3", "IFNGR1", "GBP5", "USP38", "IRF2", "IRF1", "IFNGR1", "IFI35", "IRF8", "IFITM3", "IRF5", "IFI6", "IRF1", "STING1", "IRF5", "IFI27", "IFNAR2", "LUARIS", "STING1", "ABHD17A", "CSK", "MAP3K11", "DUSP1", "DUSP2", "DUSP6", "MAPK7", "MAPK8", "MAP2K1", "MAP3K8", "MAP3K20", "MAP4K3", "MAPKAPK2", "PTK2B", "RELL1", "MINK1", "BRAF", "RIPK1", "MAPK13", "MAP2K5", "MAP3K11", "DUSP16", "DUSP16", "MAP3K15", "MAPK3", "MAP3K11", "MAP4K4", "MAP3K6", "MAPKBP1", "MAP3K3", "MAPK8IP1", "MINK1", "MAP4K4", "MAP4K3", "RIPK1", "MAP2K3", "MAP4K4", "MAP4K1", "MAP2K6", "MAP2K3", "MAPK15", "MAP2K2", "MAP4K2", "MAP2K1", "MAPKAP1", "MAPK10", "MAP4K1", "FOSL2", "FOSL1", "JUND", "JUN", "JUNB", "JUND", "JDP2", "FOSL2", "FOS", "JAK1", "STAT3", "STAT4", "JAK1", "STAT2", "STAT4", "STAT6", "JAK2", "JAK3", "STAT4", "JAK2", "JAK2", "JAK1", "STAT5A", "CCL3", "CX3CR1", "CCL3L1", "CXCL16 ", "CXCR2", "CCL22", "CCL26", "CXCR3", "CCL17", "CCR7", "CXCL12", "XCR1", "HIST1H1C", "HIST1H1D", "HIST1H1E", "H2AFZ", "H2BC5", "H2BC18", "H4C13", "H2BC15", "H3C2", "H2AC14", "H2AC4", "H4C8", "H2AC13", "H3-3A", "H2BC10", "H4C5", "H3-3B", "H2AC25", "H1-4", "KAT6A", "BRD1", "MSL2", "KAT2B", "KAT2A", "KAT2B", "BRD1", "MSL2", "KAT5", "HDAC5", "HDAC7", "HDAC9", "HDAC10", "HDAC4", "HDAC4", "HDAC11", "METTL23", "ASH1L", "PRDM2", "SETD2", "ASH1L", "SUV39H1", "ASH2L", "KMT2B", "SETD7", "PRMT2", "KMT2C", "KDM3A", "KDM2B", "KDM2B", "KDM5B", "KDM2A", "KDM7A", "KDM2A", "KDM8", "KDM4A", "KDM4B", "KDM2B", "KDM6A", "KDM4C", "KDM4A", "KDM4B", "KDM4E", "KDM5A")
 important_overall_magical_df <- overall_magical_df[overall_magical_df$Gene_symbol %in% important_magical_genes,]
 important_magical_gene_overlap_df <- magical_gene_overlap_df[magical_gene_overlap_df$Gene_Name %in% important_magical_genes,]
-write.table(important_magical_gene_overlap_df, file = "C:/Users/wat2/Desktop/important_magical_gene_overlap.tsv", sep = "\t", row.names = FALSE)
+write.table(important_magical_gene_overlap_df, file = "C:/Users/willi/Desktop/important_magical_gene_overlap.tsv", sep = "\t", row.names = FALSE)
