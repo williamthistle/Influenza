ae7968d23af6b95d8a7b680f0c248aba5a547f23
William Thistlethwaite <williamthistle@gmail.com>
2024-08-05 13:04:49 -0400

    Update 16.check_annotation_of_peak.R

--- Vaccitech_Paper/home/snATAC-Seq/16.check_annotation_of_peak.R
+++ Vaccitech_Paper/home/snATAC-Seq/16.check_annotation_of_peak.R
@@ -4,11 +4,15 @@ source(paste0(base_dir, "00.setup.R"))
 
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 # Peak to check
-# chr12:121469731-121470131: H3K4me1
-# chr12:121491198-121491598: H3K4me3
-# chr12:121464402-121464802: H3K36me3
-# chr12:121461800-121461800: hypermethylation
-# chr12:121436478-121436878: H3K9me3
+# KDM2B
+# First peak: chr12:121546911-121547411 (Intron 6 for standard transcript ENST00000377071, promoter for ENST00000538503)
+# Second peak: chr12:121580095-121580595 (Promoter for standard transcript ENST00000377071, promoter for ENST00000377069)
+# H3K4me1 negative: chr12:121469731-121470131 (Intron 12 for standard transcript ENST00000377071, promoter for ENST00000538243) - 110.9kb
+# H3K4me3 negative: chr12:121491198-121491598 (Intron 12 for standard transcript ENST00000377071) - 89.4 kb
+# H3K9me3 negative: chr12:121436478-121436878 (Intron 22 for standard transcript ENST00000377071) - 144.1 kb
+# H3K36me3 positive: chr12:121464402-121464802 (Intron 12 for standard transcript ENST00000377071) - 116.2 kb
+# snME positive: chr12:121461800-121461800 (Intron 12 for standard transcript ENST00000377071) - 119.2 kb
+# Gene TSS: 121581023
 
 checking_peak <- data.frame(seqnames = "chr12", start = "121436478", end = "121436878")
 checking_peak_annotated <- annotatePeak(makeGRangesFromDataFrame(checking_peak), TxDb = txdb, annoDb = "org.Hs.eg.db")
